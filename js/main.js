a8c66ee016a564d92d27413ab0857b12cbca79ca
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-06-26 17:15:49 +0200

    feat: add data array

--- js/main.js
+++ js/main.js
@@ -4,12 +4,27 @@
   createApp({
     data() {
       return {
-        title:"to do list",
-        items:[
-            "fare la spesa",
-            "mangiare",
-            "dormire"
-        ]
-      }
+            title:"to do list",
+            items:[
+                {
+                    text:"fare la spesa",
+                    done: "true"
+                },
+                {
+                    text:"dormire",
+                    done: "false"
+                },
+                {
+                    text:"mangiare",
+                    done: "true"
+                },
+                {
+                    text:"fare sport",
+                    done: "true"
+                }       
+                
+            ]
+            
+        }
     }
-  }).mount('#container')
+}).mount('#container')
